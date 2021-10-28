eb0152450c6a52baca942ec30753f239f918adf1
marsloy <marcosepr21@gmail.com>
2021-10-28 16:05:17 -0400

    [UPDATE] Google Analytics

--- src/pages/_document.js
+++ src/pages/_document.js
@@ -33,6 +33,18 @@ export default class MyDocument extends Document {
         return (
             <Html {...this.helmetHtmlAttrComponents}>
                 <Head>{this.helmetHeadComponents}</Head>
+                <script async src="https://www.googletagmanager.com/gtag/js?id=G-JYN3GR2GFW"></script>
+
+                <script
+                    dangerouslySetInnerHTML={{
+                        __html: ` window.dataLayer = window.dataLayer || [];
+                    function gtag(){dataLayer.push(arguments);}
+                    gtag('js', new Date());
+    
+                    gtag('config', 'G-JYN3GR2GFW');`
+                    }}
+                />
+
                 <body {...this.helmetBodyAttrComponents}>
                     <Main />
                     <script src={withPrefix('js/plugins.js')} />
